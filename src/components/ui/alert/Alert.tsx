82218698d2d3f9dcf37cbb9b49d90e9b9b69e52c
ADROEX .TSX <ali.w01rk@gmail.com>
2025-09-14 21:12:22 +0330

    not found page added (404)

--- src/components/ui/alert/Alert.tsx
+++ src/components/ui/alert/Alert.tsx
@@ -6,7 +6,7 @@ type AlertProps = { title: string };
 const Alert = ({ title }: AlertProps) => {
   return (
     <div className="px-4 w-full md:max-w-1/2 m-auto">
-      <p className="capitalize text-base md:text-4xl font-bold bg-primary py-2 px-4 rounded-md border-2 border-primary-foreground flex items-center justify-center gap-2">
+      <p className="capitalize text-base md:text-4xl font-bold bg-primary py-2 px-4 rounded-md border-2 border-primary-foreground flex items-center justify-center gap-2 transform-gpu will-change-transform">
         <IoIosWarning className="size-4 md:size-9" />
         {title}
         <IoIosWarning className="size-4 md:size-9" />
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,35 @@
+import useFadeUpAnimation from "@/animations/useFadeUpAnimation";
+import PageBreadcrumb from "@/components/ui/page-breadcrumb";
+import SectionHeader from "@/components/ui/section-header";
 import React from "react";
 
+const FADE_UP_DATA_ANIMATE = "#not-found_lines";
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  const { containerRef, imageRef } = useFadeUpAnimation({
+    fadeUpDataAnimate: FADE_UP_DATA_ANIMATE,
+  });
+
+  return (
+    <main className="space-y-20 md:space-y-40" ref={containerRef}>
+      <PageBreadcrumb title="page not found" />
+
+      <section className="px-4">
+        <SectionHeader
+          linesDataAnimate={FADE_UP_DATA_ANIMATE}
+          title="the page you are looking for"
+          text="does not exist"
+        />
+
+        <img
+          className="w-60 h-60 md:w-96 md:h-96 object-cover object-center m-auto"
+          src="/image/not-found.png"
+          alt="not-found-image"
+          ref={imageRef}
+        />
+      </section>
+    </main>
+  );
 };
 
 export default NotFoundPage;
