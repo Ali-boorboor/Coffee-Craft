05c7eab1fbd5b31f76bc4a2afb20ca14d7148de9
ADROEX .TSX <ali.w01rk@gmail.com>
2025-09-15 14:27:20 +0330

    search result page edited

--- src/components/ui/alert/Alert.tsx
+++ src/components/ui/alert/Alert.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { IoIosWarning } from "react-icons/io";
 
-type AlertProps = { title: string };
+type AlertProps = { title: string; dataAnimate?: string };
 
-const Alert = ({ title }: AlertProps) => {
+const Alert = ({ title, dataAnimate }: AlertProps) => {
   return (
-    <div className="px-4 w-full md:max-w-1/2 m-auto">
+    <div className="px-4 w-full md:max-w-1/2 m-auto" data-animate={dataAnimate}>
       <p className="capitalize text-base md:text-4xl font-bold bg-primary py-2 px-4 rounded-md border-2 border-primary-foreground flex items-center justify-center gap-2 transform-gpu will-change-transform">
         <IoIosWarning className="size-4 md:size-9" />
         {title}
--- src/features/search/components/SearchResult.tsx
+++ src/features/search/components/SearchResult.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Alert from "@/components/ui/alert/Alert";
 import ProductCard from "@/components/ui/product-card/ProductCard";
 import SectionHeader from "@/components/ui/section-header";
 import FilterButtons, {
@@ -6,15 +7,21 @@ import FilterButtons, {
   useMenuFilterStore,
 } from "@/features/menu-filter";
 import { Product } from "@/types";
+import useFadeUpAnimation from "@/animations/useFadeUpAnimation";
 
 type SearchResultProps = {
   matchedProducts: Product[];
 };
 
 const ITEMS_DATA_ANIMATE = "#search-result_items";
+const FADE_UP_DATA_ANIMATE = "#search-result_lines";
 
 const SearchResult = ({ matchedProducts }: SearchResultProps) => {
   const { menuFilterType } = useMenuFilterStore();
+
+  const { containerRef: fadeUpContainerRef, imageRef } = useFadeUpAnimation({
+    fadeUpDataAnimate: FADE_UP_DATA_ANIMATE,
+  });
   const { containerRef } = useMenuAnimations({
     itemsDataAnimate: ITEMS_DATA_ANIMATE,
     menuFilterType,
@@ -22,10 +29,32 @@ const SearchResult = ({ matchedProducts }: SearchResultProps) => {
 
   return (
     <section className="px-4 md:px-0" ref={containerRef}>
-      <div className="container m-auto flex flex-col gap-4">
-        <SectionHeader title="search result" text="found products" />
+      <div
+        className="container m-auto flex flex-col gap-4"
+        ref={fadeUpContainerRef}
+      >
+        <SectionHeader
+          linesDataAnimate={FADE_UP_DATA_ANIMATE}
+          title="search result"
+          text="found products"
+        />
 
-        <FilterButtons />
+        {matchedProducts.length ? (
+          <FilterButtons />
+        ) : (
+          <>
+            <Alert
+              dataAnimate={FADE_UP_DATA_ANIMATE}
+              title="no products found"
+            />
+            <img
+              className="w-60 h-60 md:w-80 md:h-80 object-cover object-center m-auto"
+              src="/image/flying-coffee-cup.png"
+              alt="flying-coffee-cup-image"
+              ref={imageRef}
+            />
+          </>
+        )}
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
           {menuFilterType === "all" &&
