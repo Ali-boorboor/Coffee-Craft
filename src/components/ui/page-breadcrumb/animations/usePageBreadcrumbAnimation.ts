641ca902e2e23d0a4c3488f183594b7e78a26a82
ADROEX .TSX <ali.w01rk@gmail.com>
2025-12-19 17:13:24 +0330

    text a bug

--- src/components/ui/page-breadcrumb/animations/usePageBreadcrumbAnimation.ts
+++ src/components/ui/page-breadcrumb/animations/usePageBreadcrumbAnimation.ts
@@ -1,22 +1,29 @@
-import gsap from "gsap";
-import { useRef } from "react";
-import { useGSAP } from "@gsap/react";
+import { useRef, useEffect } from "react";
 
 const usePageBreadcrumbAnimation = () => {
   const containerRef = useRef<HTMLDivElement>(null);
 
-  useGSAP(() => {
-    gsap.fromTo(
-      containerRef.current,
-      {
-        clipPath: "polygon(0% 0%,0% 0%,0% 100%,0% 100%)",
-      },
-      {
-        clipPath: "polygon(0% 0%,100% 0%,100% 100%,0% 100%)",
-        ease: "power4.inOut",
-        duration: 1,
-      }
-    );
+  useEffect(() => {
+    let gsap;
+    let ScrollTrigger;
+
+    const initAnimation = async () => {
+      gsap = (await import("gsap")).default;
+      ScrollTrigger = (await import("gsap/ScrollTrigger")).default;
+      gsap.registerPlugin(ScrollTrigger);
+
+      gsap.fromTo(
+        containerRef.current,
+        { clipPath: "polygon(0% 0%,0% 0%,0% 100%,0% 100%)" },
+        {
+          clipPath: "polygon(0% 0%,100% 0%,100% 100%,0% 100%)",
+          ease: "power4.inOut",
+          duration: 1,
+        }
+      );
+    };
+
+    initAnimation();
   }, []);
 
   return { containerRef };
--- src/database/dbConnection.ts
+++ src/database/dbConnection.ts
@@ -1,33 +1,17 @@
 import mongoose from "mongoose";
 
-interface MongooseCache {
-  connection: typeof mongoose | null;
-  promise: Promise<typeof mongoose> | null;
-}
+const connectToDB = async () => {
+  try {
+    const isConnectionReady = mongoose.connections[0].readyState;
 
-declare global {
-  var mongooseCache: MongooseCache;
-}
+    if (isConnectionReady) return false;
 
-const cached: MongooseCache = global.mongooseCache || {
-  connection: null,
-  promise: null,
+    await mongoose.connect(process.env.DB_CONNECTION_STRING!);
+
+    console.log("# Connected to db successfully !");
+  } catch (error) {
+    console.log("# Error in db connection =>", error);
+  }
 };
 
-export default async function connectToDB() {
-  if (cached.connection) {
-    return cached.connection;
-  }
-
-  if (!cached.promise) {
-    cached.promise = mongoose
-      .connect(process.env.DB_CONNECTION_STRING!)
-      .then((mongoose) => mongoose);
-  }
-
-  cached.connection = await cached.promise;
-  global.mongooseCache = cached;
-
-  console.log("# Connected to DB successfully!");
-  return cached.connection;
-}
+export default connectToDB;
