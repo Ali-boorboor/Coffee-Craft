57b8ed3988dfab820d35d3504717e6a102c33f95
ADROEX .TSX <ali.w01rk@gmail.com>
2025-12-19 18:34:11 +0330

    test gsap bug

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,24 +1,15 @@
 import React from "react";
 import Head from "next/head";
-import dynamic from "next/dynamic";
 import ProductModel from "@/models/Product";
 import connectToDB from "@/database/dbConnection";
 import jsonDataParser from "@/utils/jsonDataParser";
+import PageBreadcrumb from "@/components/ui/page-breadcrumb";
 import validationSchema from "@/utils/validators/validationSchema";
+import SearchResult from "@/features/search/components/SearchResult";
 import validateInputValues from "@/utils/validators/validateInputValues";
 import { searchValidations } from "@/validations";
 import { NextPageContext } from "next";
 import { Product } from "@/types";
-
-const PageBreadcrumb = dynamic(
-  () => import("@/components/ui/page-breadcrumb"),
-  { ssr: false }
-);
-
-const SearchResult = dynamic(
-  () => import("@/features/search/components/SearchResult"),
-  { ssr: false }
-);
 
 type SearchProps = { matchedProducts: Product[] };
 
@@ -39,40 +30,32 @@ const Search = ({ matchedProducts }: SearchProps) => {
 };
 
 export const getServerSideProps = async (context: NextPageContext) => {
-  try {
-    await connectToDB();
+  await connectToDB();
 
-    const product_name =
-      typeof context.query.product_name === "string"
-        ? context.query.product_name
-        : "";
+  const { product_name } = context.query;
 
-    const schema = validationSchema({ searchQuery: searchValidations });
+  const schema = validationSchema({ searchQuery: searchValidations });
 
-    const isValid = await validateInputValues({
-      values: { searchQuery: product_name },
-      schema,
-    });
+  const isValid = await validateInputValues({
+    values: { searchQuery: product_name },
+    schema,
+  });
 
-    if (!isValid) {
-      return {
-        props: { matchedProducts: [] },
-      };
-    }
-
-    const query = String(product_name).toLowerCase();
-
-    const matchedProducts = await ProductModel.find({
-      title: { $regex: query, $options: "i" },
-    }).lean();
-
-    const parsedMatchedProducts = jsonDataParser(matchedProducts);
-
-    return { props: { matchedProducts: parsedMatchedProducts } };
-  } catch (error) {
-    console.error("SSR error:", error);
-    return { props: { matchedProducts: [] } };
+  if (!isValid) {
+    return {
+      props: { matchedProducts: [] },
+    };
   }
+
+  const query = String(product_name).toLowerCase();
+
+  const matchedProducts = await ProductModel.find({
+    title: { $regex: query, $options: "i" },
+  }).lean();
+
+  const parsedMatchedProducts = jsonDataParser(matchedProducts);
+
+  return { props: { matchedProducts: parsedMatchedProducts } };
 };
 
 export default Search;
